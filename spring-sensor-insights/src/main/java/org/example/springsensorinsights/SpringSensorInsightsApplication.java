3de0aa962ffdf0612df921328aa8a9bca1b89e06
amin-rm <ramdhaniahmedamine@gmail.com>
2024-03-20 17:06:17 +0100

    Initialized the project

---
+++ spring-sensor-insights/src/main/java/org/example/springsensorinsights/SpringSensorInsightsApplication.java
@@ -0,0 +1,13 @@
+package org.example.springsensorinsights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSensorInsightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSensorInsightsApplication.class, args);
+	}
+
+}
